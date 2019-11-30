ba40222d25de3c80803ae2dbb301b1a9582a893c
Jonathan Disla <jond@Jonathans-MacBook-Air.local>
2019-11-30 00:24:11 -0500

    footer complete1

--- public/views/footer/footer-sec.php
+++ public/views/footer/footer-sec.php
@@ -1,30 +1,64 @@
 <div class="footer-sec">
     <div class="container">
-        <div class="row">
+        <div class="link-section row">
             <div class="col-lg-6">
                 <div class="row">
                     <div class="col-lg-6">
+                        <img src="../public/views/footer/footerimg.png" alt="fancyreslogo">
                         <p>
                             123 Northfold Ave.
                             <br>
                             Pacoima CA, 90215
                         </p>
+                        <p>
+                            <strong>Phone</strong>:(661)764-8932
+                            <br>
+                            <strong>Email</strong>: support@fancyres.com
+                        </p>
                     </div>
-                    <div class="col-lg-6">
-                        2
+                    <div class="footer-menu col-lg-6">
+                        <h5>Food Menu</h5>
+                        <ul>
+                            <li>Catering</li>
+                            <li>Family meals</li>
+                            <li>Buffet</li>
+                            <li>Meat choices</li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6">
-                <div class="row">
+            <div class="footer-menu col-lg-6">
+                <div class="opening-hours row">
                     <div class="col-lg-6">
-                        3
+                    <h5>Opening Hours</h5>
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <p>Mon-Tues:</p>
+                                <p>Wed-Thur:</p>
+                                <p>Lunch:</p>
+                                <p>Sun:</p>
+                            </div>
+                            <div class="col-lg-7">
+                            <p>6:00am - 10:00pm</p>
+                            <p>8:00am - 6:00pm</p>
+                            <p>Everyday</p>
+                            <button type="button" class="btn btn-danger">Closed</button>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-lg-6">
                         4
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="terms">
+            &copy Copyright Fancyres 2019
+            <ul>
+                <li>Terms of use</li>
+                <li>Liscense</li>
+                <li>Support</li>
+            </ul>
         </div>
     </div>
 </div>
